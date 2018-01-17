a8c787d9f05629ca0c8ae5f11946d21673534512
Andrey Popov <rainfinder@gmail.com>
2018-01-17 21:53:04 +0100

    Allow storing parton-level mtt in VarWriter
    Do not enable this by default though.

--- include/VarWriter.hpp
+++ include/VarWriter.hpp
@@ -21,6 +21,9 @@ public:
 public:
     virtual void BeginFile(TFile *) override;
     
+    /// Requests storing of some parton-level observables
+    void StorePartonLevel(bool on = true);
+    
 private:
     virtual bool ProcessEvent() override;
     
@@ -31,10 +34,14 @@ private:
     /// Non-owning pointer to plugin that performs tt reconstruction
     TTReco const *ttReco;
     
+    /// Flag showing whether parton-level variables should be stored
+    bool storePartonLevel;
+    
     /// Non-owning pointer to output tree
     TTree *outTree;
     
     // Buffers to fill the tree
     Float_t bfWeight;
     Float_t bfPtTopLep, bfPtTopHad, bfMassTT;
+    Float_t bfPartonMassTT;
 };
--- src/VarWriter.cpp
+++ src/VarWriter.cpp
@@ -4,7 +4,8 @@
 
 
 VarWriter::VarWriter(DelphesReader const *reader_, TTReco const *ttReco_):
-    reader(reader_), ttReco(ttReco_)
+    reader(reader_), ttReco(ttReco_),
+    storePartonLevel(false)
 {}
 
 
@@ -16,6 +17,15 @@ void VarWriter::BeginFile(TFile *)
     outTree->Branch("PtTopLep", &bfPtTopLep);
     outTree->Branch("PtTopHad", &bfPtTopHad);
     outTree->Branch("MassTT", &bfMassTT);
+    
+    if (storePartonLevel)
+        outTree->Branch("PartonMassTT", &bfPartonMassTT);
+}
+
+
+void VarWriter::StorePartonLevel(bool on)
+{
+    storePartonLevel = on;
 }
 
 
@@ -29,6 +39,23 @@ bool VarWriter::ProcessEvent()
     bfPtTopLep = p4TopLep.Pt();
     bfPtTopHad = p4TopHad.Pt();
     bfMassTT = (p4TopLep + p4TopHad).M();
+    
+    
+    if (storePartonLevel)
+    {
+        auto const &particles = reader->GetLHEParticles();
+        
+        TLorentzVector p4TT;
+        
+        for (auto const &p: particles)
+        {
+            if (std::abs(p.PID) == 6)
+                p4TT += p.P4();
+        }
+        
+        bfPartonMassTT = p4TT.M();
+    }
+    
     
     outTree->Fill();
     return true;
